f4efa4218c88675156111264849d7bab73fe5690
SenthilKumar M <56290175+senthilkumar909542@users.noreply.github.com>
2021-08-13 23:32:20 -0700

    Swapping

---
+++ Program/Swapping in different method/By_using_bitwise_operators.java
@@ -0,0 +1,20 @@
+package com.examples.swapping;
+
+import java.util.Scanner;
+
+public class By_using_bitwise_operators {
+    public static  void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number a=");
+        int a = sc.nextInt();
+        System.out.print("Enter the number b =");
+        int b = sc.nextInt();
+        System.out.printf("Before Swapping a=%d b =%d",a,b);
+
+
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        System.out.printf("\nAfter Swapping a=%d b =%d",a,b);
+    }
+}
